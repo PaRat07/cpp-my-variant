2d71f2e9f7434b292c294375c961561489cc6468
PaRat07 <pasha.at.phone@gmail.com>
2025-04-29 13:30:16 +0300

    Visit for std::variant

--- main.cpp
+++ main.cpp
@@ -1,133 +1,70 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
-#include <cstring>
-#include <algorithm>
-#include <chrono>
-#include <exception>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <unordered_set>
-#include <span>
-#include <utility>
+constexpr int kVariantSize = 30;
+
+#include "somevisit.h"
+#include <array>
 #include <variant>
-#include <vector>
+#include <random>
+#include <functional>
+#include <algorithm>
 #include <ranges>
+#include <chrono>
 
-#include "my-variant.h"
-#include <bit>
-#include <condition_variable>
-
-// template struct Variant<int, std::string>;
-
-// #define int int64_t
-// #include "my-visit.h"
+#include "my-visit.h"
+#include <print>
 
 using namespace std;
 
-
-namespace rng = ranges;
-namespace vie = views;
-
-
 template<typename... Ts>
-struct Overl : Ts... {
-  using Ts::operator()...;
+constexpr auto kActI = std::array {
+  std::variant<Ts...>(std::in_place_type<Ts>)...
 };
 
-struct NotTrivReloc {
-  NotTrivReloc(const NotTrivReloc&) {}
-  NotTrivReloc(NotTrivReloc&&) {}
+template<class... Ts>
+struct overloaded : Ts... { using Ts::operator()...; };
 
-  ~NotTrivReloc() {}
-};
-
-// struct NotTrivReloc {};
-
-template<typename VarT>
-static const auto kActivatedFor = [] <typename... Ts> (std::type_identity<Variant<Ts...>>) {
-  return std::array {
-    VarT(std::type_identity<Ts>{})...
-  };
-} (std::type_identity<std::remove_cvref_t<VarT>>{});
-
-signed main() {
-  volatile int noopt = 0;
-  // {
-  //   auto beg = std::chrono::steady_clock::now();
-  //   std::vector<std::variant<int, std::string, NotTrivReloc>> var_vec;
-  //   var_vec.push_back(std::variant<int, std::string, NotTrivReloc>(std::in_place_index<0>));
-  //   for (int i : views::iota(0, 10'000'000 * 0)) {
-  //     noopt += var_vec.back().visit(Overl{
-  //       [] (int) -> int { return 1; },
-  //       [] (const std::string&) -> int { return 2; },
-  //       [] (const NotTrivReloc&) -> int { std::terminate(); }
-  //     });
-  //     if (rand() % 2) {
-  //       var_vec.push_back(std::variant<int, std::string, NotTrivReloc>(std::in_place_index<0>));
-  //     } else {
-  //       var_vec.push_back(std::variant<int, std::string, NotTrivReloc>(std::in_place_index<1>));
-  //     }
-  //   }
-  //   auto end = std::chrono::steady_clock::now();
-  //   std::println("std::variant: {}", std::chrono::duration_cast<std::chrono::milliseconds>(end - beg));
-  // }
-  // {
-  //   auto beg = std::chrono::steady_clock::now();
-  //   std::vector<Variant<int, std::string, NotTrivReloc>> var_vec;
-  //   var_vec.push_back(Variant<int, std::string, NotTrivReloc>(std::in_place_index<0>));
-  //   for (int i : views::iota(0, 10'000'000 * 0)) {
-  //     noopt += Visit(Overl{
-  //       [] (int) -> int { return 1; },
-  //       [] (const std::string&) -> int { return 2; },
-  //       [] (const NotTrivReloc&) -> int { std::terminate(); }
-  //     }, var_vec.back());
-  //
-  //     if (rand() % 2) {
-  //       var_vec.push_back(Variant<int, std::string, NotTrivReloc>(std::in_place_index<0>));
-  //     } else {
-  //       var_vec.push_back(Variant<int, std::string, NotTrivReloc>(std::in_place_index<1>));
-  //     }
-  //   }
-  //   auto end = std::chrono::steady_clock::now();
-  //   std::println("Variant: {}", std::chrono::duration_cast<std::chrono::milliseconds>(end - beg));
-  // }
-  // [&] <size_t... Inds> (std::index_sequence<Inds...>) {
-    [&] <size_t Ind> (std::integral_constant<size_t, Ind>) {
-      using UnderT = decltype([] <size_t... TInds> (std::index_sequence<TInds...>) {
-        return std::type_identity<Variant<std::integral_constant<size_t, TInds>..., float, int>>{};
-      } (std::make_index_sequence<Ind>{}))::type;
-
-      static constexpr auto kOvr = [] <size_t... TInds> (std::index_sequence<TInds...>) {
-        return Overl {
-          [] (std::integral_constant<size_t, TInds>) { return TInds; }...,
-          [] (int) { return Ind + 1; },
-          [] (float) { return Ind + 2; }
-        };
-      } (std::make_index_sequence<Ind>{});
-      {
-        auto beg = std::chrono::steady_clock::now();
-        UnderT var(std::in_place_index<Ind + 1>);
-        for (int i : views::iota(0, 10'000'000)) {
-          noopt += Visit<true>(kOvr, var);
-
-          var = kActivatedFor<UnderT>[rand() % (Ind + 2)];
-        }
-        auto end = std::chrono::steady_clock::now();
-        std::println("Visit<true> for {} elems: {}", Ind, std::chrono::duration_cast<std::chrono::milliseconds>(end - beg));
+int main() {
+  [] <size_t... Inds> (std::index_sequence<Inds...>) {
+    volatile int noopt = 0;
+    volatile size_t arr[sizeof...(Inds)];
+    for (auto i : std::views::iota(0u, sizeof...(Inds))) {
+      arr[i] = i;
+    }
+    using VarT = std::variant<std::integral_constant<size_t, Inds>...>;
+    {
+      VarT var;
+      auto beg = std::chrono::steady_clock::now();
+      for (auto _ : std::views::iota(0, 10'000'000)) {
+        var = kActI<std::integral_constant<size_t, Inds>...>[rand() % sizeof...(Inds)];
+        noopt += std::visit(overloaded{
+          [] (std::integral_constant<size_t, Inds>) { return Inds; }...
+        }, var);
       }
-      {
-        auto beg = std::chrono::steady_clock::now();
-        UnderT var(std::in_place_index<Ind + 1>);
-        for (int i : views::iota(0, 10'000'000)) {
-          noopt += Visit<false>(kOvr, var);
-
-          var = kActivatedFor<UnderT>[rand() % (Ind + 2)];
-        }
-        auto end = std::chrono::steady_clock::now();
-        std::println("Visit<false> for {} elems: {}", Ind, std::chrono::duration_cast<std::chrono::milliseconds>(end - beg));
+      auto end = std::chrono::steady_clock::now();
+      std::println("std::visit: {}ms", std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count());
+    }
+    {
+      VarT var;
+      auto beg = std::chrono::steady_clock::now();
+      for (auto _ : std::views::iota(0, 10'000'000)) {
+        var = kActI<std::integral_constant<size_t, Inds>...>[rand() % sizeof...(Inds)];
+        noopt += rollbear::visit(overloaded{
+          [] (std::integral_constant<size_t, Inds>) { return Inds; }...
+        }, var);
       }
-    } (std::integral_constant<size_t, 100>{});
-  // } (std::make_index_sequence<100>{});
+      auto end = std::chrono::steady_clock::now();
+      std::println("rollbear::visit: {}ms", std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count());
+    }
+    {
+      VarT var;
+      auto beg = std::chrono::steady_clock::now();
+      for (auto _ : std::views::iota(0, 10'000'000)) {
+        var = kActI<std::integral_constant<size_t, Inds>...>[rand() % sizeof...(Inds)];
+        noopt += Visit(overloaded{
+          [] (std::integral_constant<size_t, Inds>) { return Inds; }...
+        }, var);
+      }
+      auto end = std::chrono::steady_clock::now();
+      std::println("Visit: {}ms", std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count());
+    }
+  } (std::make_index_sequence<kVariantSize>{});
 }
-
--- my-visit.h
+++ my-visit.h
@@ -6,6 +6,9 @@
 #include <functional>
 #include <stdexcept>
 #include <variant>
+#include <stdexcept>
+#include <utility>
+
 
 
 
@@ -22,7 +25,7 @@ constexpr bool CanNoexcept(std::type_identity<VisT>,
   if constexpr (sizeof...(OtherVarsTs) == 0) {
     return std::is_nothrow_invocable_v<VisT, CurActTs...>;
   } else {
-    return [] <typename... Ts> (std::type_identity<Variant<Ts...>>) {
+    return [] <typename... Ts> (std::type_identity<std::variant<Ts...>>) {
       return (CanNoexcept(std::type_identity<VisT>{},
                           std::type_identity<std::tuple<CurActTs..., decltype(std::forward_like<SepVar>(std::declval<Ts>()))>>{},
                           std::type_identity<OtherVarsTs>{}...) && ...);
@@ -30,76 +33,37 @@ constexpr bool CanNoexcept(std::type_identity<VisT>,
   }
 }
 
-template<typename VisT, typename... CurHaveTs, typename SepVarT, typename... VarsT, bool IsNoexcept, typename... VarsTs>
-consteval auto BuildFtable(std::bool_constant<IsNoexcept>,
-                          std::type_identity<VisT>,
-                          std::type_identity<std::tuple<VarsTs...>>,
-                          std::type_identity<std::tuple<CurHaveTs...>>,
-                          std::type_identity<SepVarT>,
-                          std::type_identity<VarsT>...) {
-  if constexpr (sizeof...(VarsT) == 0) {
-    return [] <typename... Ts> (std::type_identity<Variant<Ts...>>) {
-      return std::array {
-        [] <typename... AllCastTs> (std::type_identity<std::tuple<AllCastTs...>>) {
-          return +[] (VisT vis, VarsTs... vars) static noexcept(IsNoexcept) {
-              return vis(std::forward_like<VarsTs>(vars.template Get<AllCastTs>())...);
-            };
-        } (std::type_identity<std::tuple<CurHaveTs..., std::remove_cvref_t<Ts>>>{})...
-      };
-    } (std::type_identity<std::remove_cvref_t<SepVarT>>{});
-  } else {
-    return [] <typename... Ts> (std::type_identity<Variant<Ts...>>) {
-      return std::array {
-        BuildFtable(std::bool_constant<IsNoexcept>{},
-                    std::type_identity<VisT>{},
-                    std::type_identity<std::tuple<VarsTs...>>{},
-                    std::type_identity<std::tuple<CurHaveTs..., std::remove_cvref_t<Ts>>>{},
-                    std::type_identity<VarsT>{}...)...
-      };
-    } (std::type_identity<std::remove_cvref_t<SepVarT>>{});
-  }
-}
+template<typename VisT, typename... VarTs>
+constexpr bool kIsNoexc = CanNoexcept(std::type_identity<VisT>{},
+                                      std::type_identity<std::tuple<>>{},
+                                      std::type_identity<VarTs>{}...);
 
-auto &&MdAt(auto &&arr, auto sep_ind, auto... inds) {
-  if constexpr (sizeof...(inds) == 0) {
-    return std::forward<decltype(arr)>(arr)[sep_ind];
-  } else {
-    return MdAt(std::forward<decltype(arr)>(arr)[sep_ind], inds...);
-  }
-}
-template<bool BuildTable = false>
-decltype(auto) Visit(auto &&vis, auto &&...var) noexcept {
-  if constexpr (BuildTable) {
-    static constexpr bool kIsNoexcept = CanNoexcept(std::type_identity<decltype(vis)>{},
-                                                    std::type_identity<std::tuple<>>{},
-                                                    std::type_identity<decltype(var)>{}...);
-
-    static constexpr auto ftable = BuildFtable(std::bool_constant<kIsNoexcept>{},
-                                               std::type_identity<decltype(vis)>{},
-                                               std::type_identity<std::tuple<decltype(var)...>>{},
-                                               std::type_identity<std::tuple<>>{},
-                                               std::type_identity<decltype(var)>{}...);
-
-    return MdAt(ftable, var.index()...)(std::forward<decltype(vis)>(vis), std::forward<decltype(var)>(var)...);
-  } else {
-    return [] (auto &&vis, auto &&sep_var, auto&&... vars) {
-      return [&vis, &sep_var, &vars...] <typename... SepVarTs> (std::type_identity<Variant<SepVarTs...>>) {
-        return [&vis, &sep_var, &vars...] <size_t MyInd, size_t... Inds> (this auto &&self, std::index_sequence<MyInd, Inds...>) {
-          if (sep_var.index() == MyInd) {
-            if constexpr (sizeof...(vars) == 0) {
-              return vis(std::forward_like<decltype(sep_var)>(sep_var.template Get<SepVarTs...[MyInd]>()));
-            } else {
-              return Visit<false>([&] (auto&&... vals) {
-                return vis(std::forward_like<decltype(sep_var)>(sep_var.template Get<SepVarTs...[MyInd]>()), std::forward<decltype(vals)>(vals)...);
-              }, std::forward<decltype(vars)>(vars)...);
-            }
-          } else if constexpr (sizeof...(Inds) == 0) {
-            std::terminate();
+template<bool IsNoexc>
+decltype(auto) VisitImpl(auto&& vis, auto&&... var) noexcept(IsNoexc) {
+  return [] (auto &&vis, auto &&sep_var, auto&&... vars) noexcept(IsNoexc) {
+    return [&vis, &sep_var, &vars...] <typename... SepVarTs> (std::type_identity<std::variant<SepVarTs...>>) noexcept(IsNoexc) {
+      return [&vis, &sep_var, &vars...] <size_t MyInd, size_t... Inds> (this auto &&self, std::index_sequence<MyInd, Inds...>) noexcept(IsNoexc) {
+        if (sep_var.index() == MyInd) {
+          if constexpr (sizeof...(vars) == 0) {
+            return vis(std::forward_like<decltype(sep_var)>(std::get<SepVarTs...[MyInd]>(sep_var)));
           } else {
-            return self(std::index_sequence<Inds...>{});
+            return VisitImpl<IsNoexc>([&] (auto&&... vals) noexcept(IsNoexc) {
+              return vis(std::forward_like<decltype(sep_var)>(std::get<SepVarTs...[MyInd]>(sep_var)), std::forward<decltype(vals)>(vals)...);
+            }, std::forward<decltype(vars)>(vars)...);
           }
-        } (std::make_index_sequence<sizeof...(SepVarTs)>{});
-      } (std::type_identity<std::remove_cvref_t<decltype(sep_var)>>{});
-    } (std::forward<decltype(vis)>(vis), std::forward<decltype(var)>(var)...);
+        } else if constexpr (sizeof...(Inds) == 0) {
+          std::unreachable();
+        } else {
+          return self(std::index_sequence<Inds...>{});
+        }
+      } (std::make_index_sequence<sizeof...(SepVarTs)>{});
+    } (std::type_identity<std::remove_cvref_t<decltype(sep_var)>>{});
+  } (std::forward<decltype(vis)>(vis), std::forward<decltype(var)>(var)...);
+}
+
+decltype(auto) Visit(auto &&vis, auto &&...var) {
+  if ((var.valueless_by_exception() || ...)) [[unlikely]]{
+    throw std::bad_variant_access();
   }
+  return VisitImpl<kIsNoexc<decltype(vis), decltype(var)...>>(std::forward<decltype(vis)>(vis), std::forward<decltype(var)>(var)...);
 }
